7d567fec043e1df28fc0084b44a816d56a6028cb
Arvid Gussarsson <arvid.gussarsson@gmail.com>
2025-02-10 12:31:30 +0100

    feat: Adds correct imports of files etc
    Adds the correct way of finding the function for pulling/cloning a remote repo.

--- tests/test_ci_pipeline.py
+++ tests/test_ci_pipeline.py
@@ -1,10 +1,16 @@
 import unittest
 import os
+import sys
 import subprocess
 import git
 from unittest.mock import patch
 # Update line below to match the file, function and variable names that are to be implemented
 # from file_name import clone_pull_function, repo_url, repo_dir
+repo_url = "https://github.com/XXXXXXXXXXXXXXX"
+repo_dir = "./XXXXXXXXXXXXX"
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+import ci_pipeline
 
 class TestCIPipeline(unittest.TestCase):
 
@@ -15,10 +21,10 @@ class TestCIPipeline(unittest.TestCase):
         Test if the repository is cloned or pulled correctly.
         """
         if os.path.exists(repo_dir):
-            # file_name.clone_pull_function(repo_url, repo_dir) TO BE CHANGED
+            ci_pipeline.clone_pull_repo(repo_url, repo_dir)
             mock_pull.assert_called()
         else:
-            # file_name.clone_pull_function(repo_url, repo_dir) TO BE CHANGED
+            ci_pipeline.clone_pull_repo(repo_url, repo_dir)
             mock_clone.assert_called_with(repo_url, repo_dir)
 
 if __name__ == "__main__":
