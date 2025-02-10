059573da6528dc3d36c21021a318ea0f1561ed3f
Arvid Gussarsson <arvid.gussarsson@gmail.com>
2025-02-10 12:22:11 +0100

    feat: Adds function for cloning or pulling a repo
    Adds functionality for cloning or pulling a remote repository depending if it already exists locally or not.

--- src/ci_pipeline.py
+++ src/ci_pipeline.py
@@ -1,1 +1,27 @@
 # Main CI logic (compilation, testing)
+
+
+import os
+import git
+
+def clone_pull_repo(repo_url, repo_dir):
+    """
+    Function pulls or clones a GitHub repository based on a repo url. Pulls if repository already exists, otherwise clones.
+    
+    repo_url (str): The Git repository URL
+    repo_dir (str): The directory to clone the repository
+    """
+    try:
+        if os.path.exists(repo_dir):
+            repo = git.Repo(repo_dir)
+            repo.git.reset("--hard")
+            repo.git.clean("-fd")
+            repo.remotes.origin.pull()
+            print("Repository was successfully pulled.")
+        else:
+            git.Repo.clone_from(repo_url, repo_dir)
+            print("Repository was successfully cloned. Available at:", repo_dir)
+        
+    
+    except Exception as e:
+        print(f"An error occurred when cloning or pulling the remote repository: {e}")
