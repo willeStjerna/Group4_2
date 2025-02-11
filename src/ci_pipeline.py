6b4f16d7def24eb415e431b72b063861aad9d955
William Yu Stjernström <wys@kth.se>
2025-02-11 14:03:39 +0100

    refactor: clone:oull_rep now in pipeline class #37

--- src/ci_pipeline.py
+++ src/ci_pipeline.py
@@ -7,6 +7,30 @@ import git
 class CIPipeline:
     def __init__(self):
         self.logger = BuildLogger()
+
+
+    def clone_pull_repo(repo_url, repo_dir):
+        """
+        Function pulls or clones a GitHub repository based on a repo url. Pulls if repository already exists, otherwise clones.
+        If the remote repo is pulled, it is reset to the latest commit (discarding local changes) and removes untracked directories.
+        
+        repo_url (str): The Git repository URL
+        repo_dir (str): The directory to clone the repository
+        """
+        try:
+            if os.path.exists(repo_dir):
+                repo = git.Repo(repo_dir)
+                repo.git.reset("--hard")
+                repo.git.clean("-fd")
+                repo.remotes.origin.pull()
+                print("Repository was successfully pulled.")
+            else:
+                git.Repo.clone_from(repo_url, repo_dir)
+                print("Repository was successfully cloned. Available at:", repo_dir)
+            
+        
+        except Exception as e:
+            print(f"An error occurred when cloning or pulling the remote repository: {e}")
     
     def check_python_syntax(self, build_id, repo_path):
         """
@@ -53,25 +77,4 @@ class CIPipeline:
         return True
 
 
-def clone_pull_repo(repo_url, repo_dir):
-    """
-    Function pulls or clones a GitHub repository based on a repo url. Pulls if repository already exists, otherwise clones.
-    If the remote repo is pulled, it is reset to the latest commit (discarding local changes) and removes untracked directories.
-    
-    repo_url (str): The Git repository URL
-    repo_dir (str): The directory to clone the repository
-    """
-    try:
-        if os.path.exists(repo_dir):
-            repo = git.Repo(repo_dir)
-            repo.git.reset("--hard")
-            repo.git.clean("-fd")
-            repo.remotes.origin.pull()
-            print("Repository was successfully pulled.")
-        else:
-            git.Repo.clone_from(repo_url, repo_dir)
-            print("Repository was successfully cloned. Available at:", repo_dir)
-        
-    
-    except Exception as e:
-        print(f"An error occurred when cloning or pulling the remote repository: {e}")
+
