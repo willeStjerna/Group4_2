e3b9dcb7feb017dc7cd7b30f6a8091a4885a8b81
Arvid Gussarsson <arvid.gussarsson@gmail.com>
2025-02-11 12:19:05 +0100

    fix: Updates tests for notifications to new standard. #23
    Updates the unit test for testing for email notifications to match the updates in "notifications.py" (email content, body etc).
    
    Fixes #23

--- tests/test_notifications.py
+++ tests/test_notifications.py
@@ -15,10 +15,24 @@ class TestNotifications(unittest.TestCase):
         Tests if emails are sent from the CI server using mock unit tests in order to improve efficiency and safety.
         """
         mock_server = mock_smtp.return_value
-        notifications.send_email_notification("Testing testing")
+
+        commit_id = "abc123"
+        dev_email = "some_dev@example.com"
+        build_status = "Success"
+        log_content = "abcabc"
+        notifications.send_email_notification(commit_id, dev_email, build_status, log_content)
 
         mock_server.sendmail.assert_called()
         mock_server.quit.assert_called()
+
+        email = mock_server.sendmail.call_args[0]
+        sender, receiver, email_content = email
+
+        self.assertEqual(receiver, dev_email)
+
+        self.assertIn(commit_id, email_content)
+        self.assertIn(log_content, email_content)
+
 
 if __name__ == "__main__":
     unittest.main()
